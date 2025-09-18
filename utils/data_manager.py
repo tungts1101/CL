a8d6cdc29e0093961152d9a95406678f9551fc86
Tung Tran <tungts1101@gmail.com>
2025-09-18 12:46:24 +0900

    Add dataset cars

--- utils/data_manager.py
+++ utils/data_manager.py
@@ -3,7 +3,7 @@ import numpy as np
 from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms
-from utils.data import iCIFAR10, iCIFAR100, iImageNet100, iImageNet1000, iCIFAR224, iImageNetR,iImageNetA,CUB, objectnet, omnibenchmark, vtab, iTinyImageNet
+from utils.data import iCIFAR10, iCIFAR100, iImageNet100, iImageNet1000, iCIFAR224, iImageNetR,iImageNetA,CUB, objectnet, omnibenchmark, vtab, cars, iTinyImageNet
 
 
 class DataManager(object):
@@ -239,6 +239,8 @@ def _get_idata(dataset_name, args=None):
         return vtab()
     elif name == "tinyimagenet":
         return iTinyImageNet()
+    elif name == "cars":
+        return cars()
     else:
         raise NotImplementedError("Unknown dataset {}.".format(dataset_name))
 
